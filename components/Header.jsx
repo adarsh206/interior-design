331e89a6aee4011fbb60996dc8afc04a885a90a2
Adarsh Kumar <76390366+adarsh206@users.noreply.github.com>
2024-06-19 20:04:10 +0530

    header changes

--- components/Header.jsx
+++ components/Header.jsx
@@ -1,26 +1,42 @@
 'use client';
 
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import ThemeToggler from './ThemeToggler'
 import Logo from './ui/Logo';
 import Nav from './ui/Nav';
 import Mobilenavigation from './ui/Mobilenavigation';
+import { usePathname } from 'next/navigation';
 
 const Header = () => {
+
+  const [header, setHeader] = useState(false);
+  const pathname = usePathname();
+
+  useEffect(() => {
+    const scrollYPos = window.addEventListener('scroll', () => {
+      window.scrollY > 50 ? setHeader(true) : setHeader(false);
+    })
+    // remove Events
+    return () => window.removeEventListener('scroll', scrollYPos);
+  });
+
   return (
-    <header>
+    <header className={`${ header ? 'py-4 bg-tertiary shadow-lg dark:bg-accent' : 'py-6 dark:bg-transparent'} sticky top-0 z-30 transition-all ${pathname === '/' && 'bg-[#fff]'}`}>
         <div className='container mx-auto'>
-            <div className='flex items-center justify-between'>
+            <div className='flex items-center justify-evenly'>
                 {/**Logo Section */}
                 <Logo />
-                <div>
+                <div className='flex items-center gap-x-6'>
                   {/** Nav Section */}
-                   <Nav />
+                   <Nav containerStyle='hidden xl:flex gap-x-8 items-center' 
+                   linkStyles='relative hover:text-primary transition-all'
+                   underlineStyles='absolute left-0 top-full h-[2px] bg-primary w-full'
+                   />
                     {/** Theme Toggler */}
                     <ThemeToggler />
 
                     {/**Mobile Navigation */}
-                    <div>
+                    <div className='xl:hidden'>
                       <Mobilenavigation />
                     </div>
                 </div>
--- components/ui/Nav.jsx
+++ components/ui/Nav.jsx
@@ -1,8 +1,38 @@
 import React from 'react'
+import { usePathname } from 'next/navigation';
+import { motion } from "framer-motion";
+import Link from 'next/link';
 
-const Nav = () => {
+const links = [
+  {path: '/', name:'Home'},
+  {path: '/about', name:'About'},
+  {path: '/gallery', name:'Gallery'},
+  {path: '/projects', name:'Projects'},
+  {path: '/contact', name:'Contact'},
+]
+const Nav = ({containerStyle, linkStyles, underlineStyles }) => {
+  const path = usePathname();
   return (
-    <div>Nav</div>
+    <nav className={`${containerStyle}`}>
+      {
+        links.map((link, index) => {
+          return (
+            <Link href={link.path} key={index} className={`uppercase ${linkStyles}`}>
+            {link.path === path && (
+              <motion.span 
+              initial={{ y:"-100%" }}
+              animate={{ y: 0}}
+              transition={{ type: 'tween' }}
+              layoutId='underline'
+              className={`${underlineStyles}`}
+              />
+            )}  
+            {link.name}
+            </Link>
+          )
+        })
+      }
+    </nav>
   )
 }
 
