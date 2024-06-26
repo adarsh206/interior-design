62b4bc1c76e87102f041aa03af5c46fd08c4e187
Adarsh Kumar <adarsh2061998@gmail.com>
2024-06-26 19:57:04 +0530

    Update page.jsx

--- app/about/page.jsx
+++ app/about/page.jsx
@@ -19,7 +19,7 @@ const page = () => {
 
   return (
     <div>
-      <div className="bg-[url('/image/whoweare.jpg')] bg-center bg-cover ">
+      <div className="bg-[url('./image/whoweare.jpg')] bg-center bg-cover ">
         <h1 className='container py-32 text-6xl font-semibold text-black tracking-widest text-center lg:py-64'>Who are we?</h1>
       </div>
 
